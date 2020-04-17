f5d90f0ccfaea645c376980329ed00b966fdafe3
jarek0233 <46027965+jarek0233@users.noreply.github.com>
2020-04-17 20:28:50 +0200

    Add files via upload

---
+++ logowanie_system/logowanie_system/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient
+
+namespace logowanie_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection();
+            sqlcon.ConnectionString = "//adres serwera;" +
+                              "database=//nazwa bazy danych;" +
+                              "user id=//nazwa uzytkownika;" +
+                              "password=//hasło do bazy danych;";
+            string query = "Select * from loginy where Username = '" + textBox2.Text.Trim() + "' and Password = '" + textBox1.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if (dtbl.Rows.Count > 0)
+            {
+                menu objFrmMain = new menu();
+                this.Hide();
+                objFrmMain.Show();
+            }
+            else
+            {
+                MessageBox.Show("Check username or password");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '*';
+        }
+    }
+}
